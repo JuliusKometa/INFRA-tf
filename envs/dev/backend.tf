38352b50c7c99674a3edc07f6fb33155e50b3290
Yengwia <yengwia@nunu-yengwia.local>
2026-08-01 00:00:42 -0500

    Initial commit

---
+++ envs/dev/backend.tf
@@ -0,0 +1,12 @@
+# backend configuration
+
+terraform {
+  backend "s3" {
+    bucket = "zen-pharma-terraform-state-ranjiniganeshan"
+    key    = "envs/dev/terraform.tfstate"
+    region = "us-east-1"
+    encrypt      = true
+    use_lockfile = true   # S3 native locking
+
+  }
+}
